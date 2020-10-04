40ac3f6f9f9d32e0df2f644845c96128a3e56a63
ne2030 <erguono@gmail.com>
2020-10-04 17:14:48 +0900

    name svg 반영

--- src/component/namecard/index.js
+++ src/component/namecard/index.js
@@ -1,7 +1,7 @@
 import { html } from 'fxjs';
 import './namecard.css';
 
-export const makeNameCard = (data) => html`
+export const makeNameCard = (data, nameSvg) => html`
   <div class="namecard">
     <div class="head">
       <div class="logo">${data.logo ? `<img src="${data.logo}" alt="" />` : ''}</div>
@@ -10,7 +10,7 @@ export const makeNameCard = (data) => html`
         <p>${data.mobile}</p>
       </div>
     </div>
-    <div class="name">${data.name}</div>
+    <div class="name">${nameSvg}</div>
     <div class="address">${data.address}</div>
   </div>
 `;
