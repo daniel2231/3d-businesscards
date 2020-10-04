a5eab20e598dc7b5f00e186753b4311e67344d4b
daniel2231 <kangheon2231@gmail.com>
2020-10-04 17:29:37 +0900

    changed texttosvg

--- src/util/textToSvg.js
+++ src/util/textToSvg.js
@@ -1,9 +1,13 @@
 import TextToSVG from 'text-to-svg';
 
 export const svgConvert = (name) => {
-  TextToSVG.load('public/font/BadScript-Regular.ttf', function (err, textToSVG) {
-    const svg = textToSVG.getSVG(name);
-    console.log(svg)
-    return svg
+  return new Promise(function (resolve, reject) {
+    TextToSVG.load('public/font/BadScript-Regular.ttf', function (err, textToSVG) {
+      if (err) {
+        reject(err);
+      }
+      const svg = textToSVG.getSVG(name);
+      resolve(svg);
+    });
   });
-}
+};
